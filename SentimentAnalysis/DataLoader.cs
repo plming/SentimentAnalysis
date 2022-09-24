a3f3f21ec820ce4d0e13ead16a7d4626fc091f35
Kim Hyeonseok <kim010322@gmail.com>
2022-09-24 19:11:43 +0900

    rename Reader as DataLoader

--- SentimentAnalysis/DataLoader.cs
+++ SentimentAnalysis/DataLoader.cs
@@ -5,13 +5,20 @@ using System.Globalization;
 
 namespace SentimentAnalysis
 {
+    /// <summary>
+    /// 파일로부터 데이터셋을 불러옵니다.
+    /// </summary>
     public static class DataLoader
     {
+        /// <summary>
+        /// DataLoader 클래스가 읽을 파일이 위치하는 디렉토리 경로
+        /// </summary>
         public static string DATA_DIR_PATH { get; } = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "data");
 
         /// <summary>
         /// csv 파일로부터 댓글을 불러옵니다.
-        /// csv 파일의 컬럼은 label, text 순서입니다.
+        /// 컬럼은 label, text 순서로 있어야 합니다.
+        /// label은 P 또는 N이여야만 합니다.
         /// </summary>
         /// <param name="path">csv 파일의 경로</param>
         /// <returns></returns>
@@ -29,14 +36,14 @@ namespace SentimentAnalysis
             {
                 while (csv.Read())
                 {
-                    bool labelExists = csv.TryGetField(0, out string labelString);
+                    bool labelExists = csv.TryGetField(0, out string firstColumn);
                     Debug.Assert(labelExists);
 
-                    bool textExists = csv.TryGetField(1, out string text);
+                    bool textExists = csv.TryGetField(1, out string secondColumn);
                     Debug.Assert(textExists);
 
                     Label label;
-                    switch (labelString)
+                    switch (firstColumn)
                     {
                         case "P":
                             label = Label.POSITIVE;
@@ -46,10 +53,10 @@ namespace SentimentAnalysis
                             break;
                         default:
                             Debug.Fail("unknown label");
-                            throw new InvalidDataException();
+                            throw new InvalidDataException("unknown label");
                     }
 
-                    comments.Add(new Comment(text, label));
+                    comments.Add(new Comment(secondColumn, label));
                 }
             }
 
