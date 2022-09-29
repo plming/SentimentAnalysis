af15c04876d5af5f07cfff1a6fb50f1dff229e2d
Kim Hyeonseok <kim010322@gmail.com>
2022-09-29 18:36:42 +0900

    점수 모델 구현

--- SentimentAnalysis/ScoreModel.cs
+++ SentimentAnalysis/ScoreModel.cs
@@ -11,8 +11,18 @@
 
         public override Label Predict(Comment x)
         {
+            int sumFrequency = 0;
+            int sumScoreFrequencyProduct = 0;
+            foreach (string word in x.Words)
+            {
+                int frequency = x.GetFrequency(word);
+                
+                sumFrequency += frequency;
+                sumScoreFrequencyProduct += polarityScores.GetValueOrDefault(word, 0) * frequency;
+            }
 
-            throw new NotImplementedException();
+            const double THRESHOLD = 0.4;
+            return (double)sumScoreFrequencyProduct / sumFrequency > THRESHOLD ? Label.POSITIVE : Label.NEAGTIVE;
         }
     }
 }
