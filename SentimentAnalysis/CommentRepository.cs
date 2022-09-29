2c722528fe954b356839c2209a6148a93f2b67e6
Kim Hyeonseok <kim010322@gmail.com>
2022-09-29 18:36:53 +0900

    CommentRepository 구현

---
+++ SentimentAnalysis/CommentRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SentimentAnalysis
+{
+    public class CommentRepository
+    {
+        private List<Comment> comments;
+
+        public CommentRepository(string path)
+        {
+            Random random = new();
+
+            // load commments and shuffle
+            comments = DataLoader.LoadCommentsFromCsvFile(path)
+                                 .OrderBy(_ => random.Next())
+                                 .ToList();
+
+        }
+
+        public bool TrySplit(double ratio, out List<Comment> trainData, out List<Comment> testData)
+        {
+            trainData = new();
+            testData = new();
+
+            for (int i = 0; i < comments.Count; i++)
+            {
+                if (i < comments.Count * ratio)
+                {
+                    trainData.Add(comments[i]);
+                }
+                else
+                {
+                    testData.Add(comments[i]);
+                }
+            }
+
+            Debug.Assert(trainData.Count + testData.Count == comments.Count);
+
+            return true;
+        }
+
+        public int Count(Label label)
+        {
+            return comments.Where(c => c.Label == label).Count();
+        }
+    }
+}
+
--- SentimentAnalysis/Program.cs
+++ SentimentAnalysis/Program.cs
@@ -6,59 +6,38 @@ namespace SentimentAnalysis
     {
         public static void Main()
         {
-            Random random = new Random();
             Stopwatch stopwatch = new();
-
             stopwatch.Start();
 
-            List<Comment> comments = DataLoader.LoadCommentsFromCsvFile(Path.Combine(DataLoader.DATA_DIR_PATH, "comments.csv"))
-                                               .OrderBy(_ => random.Next())
-                                               .ToList();
+            CommentRepository repository = new(Path.Combine(DataLoader.DATA_DIR_PATH, "comments.csv"));
 
-            Console.WriteLine("댓글 데이터를 섞었습니다");
+            repository.TrySplit(ratio: 0.7,
+                                out List<Comment> trainDataSet,
+                                out List<Comment> testDataSet);
 
-            var positiveComments = comments.Where(c => c.Label == Label.POSITIVE);
-            var negativeComments = comments.Where(c => c.Label == Label.NEAGTIVE);
+            Model[] models = { new KnnModel(trainDataSet), new ScoreModel() };
 
-            int minCount = Math.Min(positiveComments.Count(), negativeComments.Count());
-
-            #region SPLIT_INTO_TRAIN_TEST
-            const float TRAIN_RATIO = 0.8F;
-            List<Comment> trainDataSet = new List<Comment>();
-            List<Comment> testDataSet = new List<Comment>();
-            for (int i = 0; i < comments.Count; i++)
+            foreach (Model model in models)
             {
-                if (i < comments.Count * TRAIN_RATIO)
+                int numMatched = 0;
+
+                foreach(Comment comment in testDataSet)
                 {
-                    trainDataSet.Add(comments[i]);
+                    Label predict = model.Predict(comment);
+
+                    if (comment.Label == predict)
+                    {
+                        numMatched++;
+                    }
                 }
-                else
-                {
-                    testDataSet.Add(comments[i]);
-                }
+
+                Console.WriteLine($"[{model.GetType().Name}] 정확도(%): {(double)numMatched / testDataSet.Count * 100}");
             }
-            #endregion
 
-            Model knnModel = new KnnModel(trainDataSet);
-            Model scoreModel = new ScoreModel();
-
-            int numMatched = 0;
-            for (int i = 0; i < testDataSet.Count; i++)
-            {
-                Label predict = knnModel.Predict(testDataSet[i]);
-
-                if (testDataSet[i].Label == predict)
-                {
-                    numMatched++;
-                }
-                else
-                {
-                    Console.WriteLine($"#{i} 예측: {predict} / 실제: {testDataSet[i].Label}");
-                }
-            }
             stopwatch.Stop();
+            Console.WriteLine($"긍정 갯수: {repository.Count(Label.POSITIVE)}");
+            Console.WriteLine($"부정 갯수: {repository.Count(Label.NEAGTIVE)}");
 
-            Console.WriteLine($"정확도(%): {(double)numMatched / testDataSet.Count * 100}");
             Console.WriteLine($"수행 시간: {stopwatch.Elapsed}");
         }
     }
